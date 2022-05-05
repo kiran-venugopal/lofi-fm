fa3250f53b517599903d8500450769a52761077e
Kiran V <kiranvenugopal07@gmail.com>
2022-05-05 16:56:44 +0000

    initial code

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <iframe
+        width="1753"
+        height="773"
+        src="https://www.youtube.com/embed/MCkTebktHVc"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
