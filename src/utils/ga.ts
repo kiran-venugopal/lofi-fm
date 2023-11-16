91dcf2aa115072dc1832bd3814d41c265d1d4508
Kiran V <62516337+kiran-venugopal@users.noreply.github.com>
2023-11-16 04:13:03 +0000

    minor fix

--- src/utils/ga.ts
+++ src/utils/ga.ts
@@ -1,7 +1,14 @@
 async function initGA() {
   try {
-    const ReactGA = await import("react-ga4");
-    ReactGA.initialize("G-99BB8YJNGZ")
+    const { default: ReactGA } = await import("react-ga4");
+    ReactGA.initialize("G-99BB8YJNGZ");
+
+    // Send pageview with a custom path
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+      title: "Page Loaded",
+    });
   } catch (err) {
     console.error("Error in GA config!", err);
   }
